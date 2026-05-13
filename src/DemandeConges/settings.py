d6d0a08aae86806f74def3139063112b38feed74
KevinArnaud32 <arnaudkevin32@gmail.com>
2026-05-13 13:47:17 +0000

    config url et ajout config static du projet global dans settings

--- src/DemandeConges/settings.py
+++ src/DemandeConges/settings.py
@@ -131,4 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static/',
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 AUTH_USER_MODEL = 'employe.Utilisateur'
--- src/DemandeConges/urls.py
+++ src/DemandeConges/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from tkinter.font import names
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from dashboard.views import dashboard
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', dashboard),
+    path('permission/', include('demande.urls.permission_url')),
+    path('conges/', include('demande.urls.conge_url')),
+    path('repos_maladie/',include('demande.urls.repos_maladie_url')),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
