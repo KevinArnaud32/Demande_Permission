1ab2479c6193af8ae6de0c8f8934fe9df3676395
KevinArnaud32 <arnaudkevin32@gmail.com>
2026-06-08 11:20:46 +0000

    modification du mot passe à la première connexion

--- src/authentification/urls.py
+++ src/authentification/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
-from .views import logout_view, login_view
+from .views import logout_view, login_view, change_password_first
 
 
 urlpatterns = [
-    path(
-        '',
-        login_view,
-        name='login'
-    ),
-
-    path(
-        'logout/',
-        logout_view,
-        name='logout'
-    ),
+    path('', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('change_password_first/', change_password_first, name='change_password_first')
 ]
--- src/authentification/views.py
+++ src/authentification/views.py
@@ -6,6 +6,20 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def login_view(request):
 
+    if request.user.is_authenticated:
+        if request.user.role == "admin":
+            return redirect('admin_dashboard')
+
+        elif request.user.role == "rh":
+            return redirect('rh_dashboard')
+
+        elif request.user.role == "Manager":
+            return redirect('manager_dashboard')
+
+        else:
+            return redirect('employe_dashboard')
+
+
     error = None
 
     if request.method == 'POST':
@@ -22,13 +36,16 @@ def login_view(request):
 
             login(request, user)
 
+            if user.first_login:
+                return redirect('change_password_first')
+
             if user.role == "admin":
                 return redirect('admin_dashboard')
 
             elif user.role == "rh":
                 return redirect('rh_dashboard')
 
-            elif user.role == "Manager":
+            elif user.role == "manager":
                 return redirect('manager_dashboard')
 
             else:
@@ -52,3 +69,33 @@ def logout_view(request):
     logout(request)
 
     return redirect('login')
+
+
+
+@login_required
+def change_password_first(request):
+
+    if request.method == "POST":
+
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 == password2:
+
+            user = request.user
+            user.set_password(password1)
+
+            user.first_login = False
+            user.save()
+
+            return redirect("login")
+
+        else:
+
+            context = {
+                'error': "Les mots de passe ne correspondent pas",
+            }
+
+            return render(request, "auth/change_password.html")
+
+    return render(request, "auth/change_password.html")
