923d8165b207b6889ede99ff0c7b442867e4c076
KevinArnaud32 <arnaudkevin32@gmail.com>
2026-06-08 11:32:20 +0000

    module demande

--- src/demande/admin.py
+++ src/demande/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from demande.models.conges_model import Conges
+from demande.models.permission_model import Permission
+from demande.models.repos_maladie_model import ReposMaladie
+from demande.models.solde_conges_model import SoldesConges
+from demande.models.validation_model import Validation
+
 # Register your models here.
+admin.site.register(Conges)
+admin.site.register(Permission)
+admin.site.register(ReposMaladie)
+admin.site.register(SoldesConges)
+admin.site.register(Validation)
