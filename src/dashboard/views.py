49bbd34b6a38bf6d063b50541a6212f41875512c
KevinArnaud32 <arnaudkevin32@gmail.com>
2026-06-12 10:36:35 +0000

    modification dashboard en fonction des rôles

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,113 +1,32 @@
 from django.contrib.auth.decorators import login_required
+
 from core.decorators import role_required
 from employe.models.departement_model import Departement
 from employe.models.employe_model import Employe
-from demande.models.conges_model import Conges
-from demande.models.permission_model import Permission
-from demande.models.repos_maladie_model import ReposMaladie
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from employe.models.utilisateur_model import Utilisateur
 
 
 # Create your views here.
 @login_required
-@role_required('rh')
 def dashboard(request):
 
-    user = request.user
+    if request.user.role == "admin":
+        return redirect('admin_dashboard')
 
-    permissions = Permission.objects.none()
+    elif request.user.role == "rh":
+        return redirect('rh_dashboard')
 
-    conges = Conges.objects.none()
-
-    repos = ReposMaladie.objects.none()
-
-    # ================= EMPLOYE =================
-
-    if user.role == 'employe':
-
-        permissions = Permission.objects.filter(
-            employe=user.employe
-        )
-
-        conges = Conges.objects.filter(
-            employe=user.employe
-        )
-
-        repos = ReposMaladie.objects.filter(
-            employe=user.employe
-        )
-
-    # ================= MANAGER =================
-
-    elif user.role == 'manager':
-
-        subordonnes = Employe.objects.filter(
-            superieur=user.employe
-        )
-
-        permissions = Permission.objects.filter(
-            employe__in=subordonnes
-        )
-
-        conges = Conges.objects.filter(
-            employe__in=subordonnes
-        )
-
-        repos = ReposMaladie.objects.filter(
-            employe__in=subordonnes
-        )
-
-    # ================= RH / ADMIN =================
-
-    elif user.role in ['rh', 'ADMIN']:
-
-        permissions = Permission.objects.all()
-
-        conges = Conges.objects.all()
-
-        repos = ReposMaladie.objects.all()
-
-    context = {
-
-        'permissions_count': permissions.count(),
-
-        'conges_count': conges.count(),
-
-        'repos_count': repos.count(),
-
-        'permissions_attente': permissions.filter(
-            statut='en attente'
-        ).count(),
-
-        'conges_attente': conges.filter(
-            statut='en attente'
-        ).count(),
-
-        'repos_attente': repos.filter(
-            statut='en attente'
-        ).count(),
-
-        'latest_permissions': permissions.order_by(
-            '-id'
-        )[:5],
-
-        'latest_conges': conges.order_by(
-            '-id'
-        )[:5],
-
-        'latest_repos': repos.order_by(
-            '-id'
-        )[:5],
-
-    }
-
-    return render(request,'dashboard/index.html', context)
+    elif request.user.role == "manager":
+        return redirect('manager_dashboard')
+    elif request.user.role == 'employe':
+        return redirect('employe_dashboard')
 
 
 
 
 @login_required
+@role_required('admin')
 def admin_dashboard(request):
 
     nombre_employe = Employe.objects.all().count()
@@ -115,12 +34,17 @@ def admin_dashboard(request):
     nombre_manager = Utilisateur.objects.filter(role='manager').count()
     nombre_rh = Utilisateur.objects.filter(role='rh').count()
 
+    dernier_utilisateurs = Utilisateur.objects.order_by('-date_creation')[:5]
+    dernier_employes = Employe.objects.select_related('departement', 'fonction').order_by('-id')[:5]
+
 
     context = {
         'nombre_employe': nombre_employe,
         'nombre_departement': nombre_departement,
         'nombre_manager': nombre_manager,
         'nombre_rh': nombre_rh,
+        'derniers_utilisateurs': dernier_utilisateurs,
+        'derniers_employes': dernier_employes,
     }
 
     return render(request,'dashboard/admin_dashboard.html', context)
@@ -128,17 +52,20 @@ def admin_dashboard(request):
 
 
 @login_required
+@role_required('rh')
 def rh_dashboard(request):
     return render(request,'dashboard/rh_dashboard.html')
 
 
 
 @login_required
+@role_required('manager')
 def manager_dashboard(request):
     return render( request,'dashboard/manager_dashboard.html')
 
 
 
 @login_required
+@role_required('employe')
 def employe_dashboard(request):
     return render(request,'dashboard/employe_dashboard.html')
