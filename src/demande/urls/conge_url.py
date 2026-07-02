50569dd53e29eac6654ca94b9a0a3c0ab2561640
KeivnArnaud32 <arnaudkevin32@gmail.com>
2026-07-02 16:47:12 +0000

    ligne valider/refuser ajouté pour conge_url

--- src/demande/urls/conge_url.py
+++ src/demande/urls/conge_url.py
@@ -1,35 +1,15 @@
 from django.urls import path
-from demande.views.conge_view import conge_list, conge_create, conge_delete, conge_detail, conge_update
+from demande.views.conge_view import *
 
 
 urlpatterns = [
-    path(
-        '',
-        conge_list,
-        name='conge_list'
-    ),
 
-    path(
-        'create/',
-        conge_create,
-        name='conge_create'
-    ),
+    path('', conge_list, name='conge_list'),
+    path('create/', conge_create, name='conge_create'),
+    path('details/<int:pk>/', conge_detail, name='conge_detail'),
+    path('update/<int:pk>/', conge_update, name='conge_update'),
+    path('delete/<int:pk>/', conge_delete, name='conge_delete'),
+    path('valider/<int:pk>/', valider_conge, name='valider_conge'),
+    path('refuser/<int:pk>/', refuser_conge, name='refuser_conge'),
 
-    path(
-        'detail/<int:pk>/',
-        conge_detail,
-        name='conge_detail'
-    ),
-
-    path(
-        'update/<int:pk>/',
-        conge_update,
-        name='conge_update'
-    ),
-
-    path(
-        'delete/<int:pk>/',
-        conge_delete,
-        name='conge_delete'
-    ),
 ]
