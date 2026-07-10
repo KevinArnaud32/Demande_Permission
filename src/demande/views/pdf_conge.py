d83ee2ad790d2e93dd6492c83e7163cb2d35cb85
KeivnArnaud32 <arnaudkevin32@gmail.com>
2026-07-10 16:57:58 +0000

    pdf les models repos maladie et permission

--- src/demande/views/pdf_conge.py
+++ src/demande/views/pdf_conge.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from utils.generer_pdf_conge import generer_pdf_conge
+from utils.generer_pdf_permission import generer_pdf_permission
+from utils.generer_pdf_repos_maladie import generer_pdf_repos_maladie
 from demande.models.conges_model import Conges
+from demande.models.permission_model import Permission
+from demande.models.repos_maladie_model import ReposMaladie
 
 def conge_pdf_view(request, pk):
 
@@ -10,5 +14,30 @@ def conge_pdf_view(request, pk):
 
     response = HttpResponse(pdf_buffer, content_type='application/pdf')
     response['Content-Disposition'] = f'attachment; filename="conge_{conge.employe.nom}_{conge.employe.prenom}.pdf"'
+
+    return response
+
+
+def permission_pdf_view(request, pk):
+
+    permission = Permission.objects.get(pk=pk)
+
+    pdf_buffer = generer_pdf_permission(permission)
+
+    response = HttpResponse(pdf_buffer, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="permission_{permission.employe.nom}_{permission.employe.prenom}.pdf"'
+
+    return response
+
+
+
+def repos_maladie_pdf_view(request, pk):
+
+    repos = ReposMaladie.objects.get(pk=pk)
+
+    pdf_buffer = generer_pdf_conge(repos)
+
+    response = HttpResponse(pdf_buffer, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="repos_{repos.employe.nom}_{repos.employe.prenom}.pdf"'
 
     return response
